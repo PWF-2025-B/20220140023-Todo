1be0a203290182a994fefd02336e5bfcd434d5c4
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2025-04-24 09:39:14 +0700

    create, read, search data

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,18 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Todo;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Collection ;
+use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        //$todos = Todo::all();
-        $todos = Todo::where('user_id', Auth::id())->get();
-        dd($todos);
-        return view('todo.index');
+        $todos = Todo::where('user_id', auth()->user()->id)
+
+            ->orderBy('is_done','asc')
+
+            ->orderBy('created_at','desc')
+
+            ->get();
+     
+        //dd($todos);
+        return view('todo.index', compact('todos'));
     }
 
     public function create()
@@ -24,5 +30,43 @@ class TodoController extends Controller
     public function edit()
     {
         return view('todo.edit');
+    }
+
+    public function store(Request $request, Todo $todo)
+    {
+        $request->validate([
+
+            'title' => 'required|max:255',
+
+        ]);
+
+        //Practical
+
+        //$todo = new Todo;
+
+        //$todo->title = $request->title;
+
+        //$todo->user_id = auth()->user()->id();
+
+        //$todo->save();
+
+
+
+        // Query Builder way
+        // DB::table('todos')->insert([
+        //      'title' => $request->title,
+        //      'user_id' => auth()->user()->id,
+        //      'created_at' => now(),
+        //      'updated_at' => now(),
+        // ]);
+
+        // Eloquent Way - Readable
+
+        $todo = Todo::create([
+            'title' => ucfirst($request->title),
+            'user_id' => auth()->user()->id,
+        ]);
+
+        return redirect()->route('todo.index')->with('success', 'Todo created successfully!');
     }
 }
---
+++ screenshot/tugas5/Tugas5.md
@@ -0,0 +1,20 @@
+##Tugas 5
+
+## 1. Todo Create
+![alt text](ScreenshotCreate-2.png)
+
+
+## 2. Todo Created
+![alt text](ScreenshotCreated.png)
+
+
+
+## 3. Todo Search
+![alt text](<Screenshot Search-1.png>)
+
+
+
+
+## 4. Todo Search Result
+![alt text](SSSearchNamadpn.png)
+
