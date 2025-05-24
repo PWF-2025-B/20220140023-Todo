f3c757887ce17421258ce4d6cebee2c60450abf9
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2025-05-24 12:04:50 +0700

    mailtrap

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -23,7 +23,7 @@ Route::middleware('auth')->group(function () {
     Route::post('/todo', [TodoController::class, 'store'])->name('todo.store');
     Route::get('/todo/create',[TodoController::class,'create'])->name('todo.create');
     Route::get('/todo/{todo}/edit',[TodoController::class,'edit'])->name('todo.edit');
-Route::patch('/todo/{todo}', [TodoController::class, 'update'])->name('todo.update');
+    Route::patch('/todo/{todo}', [TodoController::class, 'update'])->name('todo.update');
     Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
     Route::patch('/todo/{todo}/uncomplete',[TodoController::class, "uncomplete"])->name('todo.uncomplete');
     Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])->name('todo.destroy');
---
+++ screenshot/Tugas10/Tugas10.md
@@ -0,0 +1,21 @@
+##  Tugas 10 Yogi Dewanto 20220140023 B ##
+
+# 1. Browser Register #
+![alt text](image-1.png)
+
+
+
+# 2. Browser Verify #
+![alt text](<Screenshot 2025-05-24 110447.png>)
+
+
+
+
+# 3. Browser Mail Trap #
+![alt text](<Screenshot 2025-05-24 110544.png>)
+
+
+
+
+# 4. DashBoard #
+![alt text](image.png)
